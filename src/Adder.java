cfb879f903693a5ae46e12148c8fafeb1ac7261c
theHalfMeal <aravindkrishna585@gmail.com>
2022-05-08 20:39:05 +0530

    Added script to handle \n as last character case

--- src/Adder.java
+++ src/Adder.java
@@ -1,10 +1,13 @@
 
 public class Adder {
 	
-	public int Add(String numbers) {
+	public int Add(String numbers) throws Exception {
 		if(numbers.isEmpty()) {
 			return 0;
 		} else if(numbers.contains("\n")) {
+			if(numbers.charAt(numbers.length() - 1) == '\n') {
+				throw new Exception("Please enter values after new line");
+			}
 			numbers = numbers.replace("\n", ",");
 		}
 		int sum = 0;
--- src/AdderTest.java
+++ src/AdderTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 public class AdderTest {
 	
 	@Test
-	public void should_return_0_if_empty_string() {
+	public void should_return_0_if_empty_string() throws Exception {
 		Adder adder = new Adder();
 		int actualResult = adder.Add("");
 		int expectedResult = 0;
@@ -14,7 +14,7 @@ public class AdderTest {
 	}
 	
 	@Test
-	public void should_return_sum_if_single_value_is_present() {
+	public void should_return_sum_if_single_value_is_present() throws Exception {
 		Adder adder = new Adder();
 		int actualResult = adder.Add("1");
 		int expectedResult = 1;
@@ -22,7 +22,7 @@ public class AdderTest {
 	}
 	
 	@Test
-	public void should_return_sum_if_multiple_values_are_present() {
+	public void should_return_sum_if_multiple_values_are_present() throws Exception {
 		Adder adder = new Adder();
 		int actualResult = adder.Add("1,2,3");
 		int expectedResult = 6;
@@ -30,20 +30,20 @@ public class AdderTest {
 	}
 	
 	@Test
-	public void should_return_sum_if_new_lines_are_present() {
+	public void should_return_sum_if_new_lines_are_present() throws Exception {
 		Adder adder = new Adder();
 		int actualResult = adder.Add("1\n2,3");
 		int expectedResult = 6;
 		assertEquals(expectedResult, actualResult);
 	}
 	
-	@Test
+	@Test()
 	public void should_throw_error_if_no_value_after_new_line() {
 		Exception exception = assertThrows(Exception.class, () -> {
 			new Adder().Add("1,\n");
 		});
 		String expectedMessage = "Please enter values after new line";
 		String actualMessage = exception.getMessage();
-		assertTrue(actualMessage.contains(expectedMessage));
+		assertEquals(expectedMessage, actualMessage);
 	}
 }
