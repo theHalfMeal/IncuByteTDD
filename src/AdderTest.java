1d649295cf8d2cd8d3c4a45f1d4cca849ce58f48
theHalfMeal <aravindkrishna585@gmail.com>
2022-05-08 19:47:11 +0530

    Commit with base test case(empty string)
    Added a test case in test file which checks for empty string.

--- src/AdderTest.java
+++ src/AdderTest.java
@@ -1,9 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class AdderTest {
 	
 	@Test
 	public void should_return_0_if_empty_string() {
-		
+		Adder adder = new Adder();
+		int actualResult = adder.Add("");
+		int expectedResult = 0;
+		assertEquals(expectedResult, actualResult);
 	}
 }
