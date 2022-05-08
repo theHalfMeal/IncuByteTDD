54cab2a339a81b26539e5550fbf61f731fd35258
theHalfMeal <aravindkrishna585@gmail.com>
2022-05-08 20:51:10 +0530

    Added script to handle delimiter test case

--- src/Adder.java
+++ src/Adder.java
@@ -4,14 +4,20 @@ public class Adder {
 	public int Add(String numbers) throws Exception {
 		if(numbers.isEmpty()) {
 			return 0;
-		} else if(numbers.contains("\n")) {
+		}
+		String delimiter = ",";
+		if(numbers.contains("//")) {
+			delimiter = String.valueOf(numbers.charAt(2));
+			numbers = numbers.substring(4);
+		}
+		if(numbers.contains("\n")) {
 			if(numbers.charAt(numbers.length() - 1) == '\n') {
 				throw new Exception("Please enter values after new line");
 			}
-			numbers = numbers.replace("\n", ",");
+			numbers = numbers.replace("\n", delimiter);
 		}
 		int sum = 0;
-		String[] tokens = numbers.split(",");
+		String[] tokens = numbers.split(delimiter);
 		for(String token : tokens) {
 			sum += Integer.parseInt(token);
 		}
